f2471513d4f600dbcd898343c44177269c10aaa6
maylvak <mayam.lucena@gmail.com>
2022-10-18 18:34:37 -0300

    Adicionando arquivo em c e bash

--- acender_ledsc.c
+++ acender_ledsc.c
@@ -13,28 +13,34 @@
 #define GPIO_verde_path "/sys/class/gpio/gpio21/"   // define o caminho para acessar a porta do led
 #define GPIO_SYSFS "/sys/class/gpio/"  // define o caminho para o sistema
 
-void alterar_GPIO(char filename[], char value[]){    // funcao para escrever o valor fornecido no arquivo que se deseja, serve tanto para acender e desligar o led quanto para habilitar ou desabilitar uma porta
+void alterar_led(char filename[], char value[]){    // funcao para escrever o valor fornecido no arquivo que se deseja, serve tanto para acender e desligar o led quanto para habilitar ou desabilitar uma porta
    FILE* fp;                           
    fp = fopen(filename, "w+");       
    fprintf(fp, "%s", value);           
    fclose(fp);                         
 }
 
-void habilitar(char GPIO_num, char GPIO_path){      // funcao para habilitar a porta associada ao led
-	alterar_led(GPIO_SYSFS "export", GPIO_num );
-	usleep(1000);
-	alterar_led(GPIO_path "direction","out");        
+void habilitar(){      // funcao para habilitar a porta associada ao led
+	alterar_led(GPIO_SYSFS "export", GPIO_amarelo_num);
+	usleep(10000);
+	alterar_led(GPIO_SYSFS "export", GPIO_vermelho_num);
+	usleep(10000);
+	alterar_led(GPIO_SYSFS "export", GPIO_verde_num);
+	usleep(10000);
+	alterar_led(GPIO_vermelho_path "direction", "out");
+	usleep(10000); 
+        alterar_led(GPIO_verde_path "direction", "out");
+	usleep(10000);
+        alterar_led(GPIO_amarelo_path "direction", "out");         
 }
 
-void desabilitar (char GPIO_num){      // funcao para desabilitar a porta associada ao led
-	alterar_led ( GPIO_SYSFS "unexport", GPIO_num );
+void desabilitar (char GPIO_num[]){      // funcao para desabilitar a porta associada ao led
+	alterar_led (GPIO_SYSFS "unexport", GPIO_num);
 }
 
 int main (){   //funcao principal que retorna um inteiro 0
-        int c = 0;  // define uma variavel que serve como contador para o laco while
-	habilitar (GPIO_vermelho_num, GPIO_vermelho_path);  // habilita a porta do led vermelho
-	habilitar (GPIO_amarelo_num, GPIO_amarelo_path);  // habilita a porta do led amarelo
-	habilitar (GPIO_verde_num, GPIO_verde_path);   // habilita a porta do led verde
+        int c = 0;  // define uma variavel que serve como contador
+	habilitar();
 	while(c<5){;   //laco de recorrencia que irá repetir 5 vezes o que esta contido nele
 		alterar_led(GPIO_vermelho_path "value", "1");   // acende o led vermelho por 2 seg
 		usleep(2000000);
