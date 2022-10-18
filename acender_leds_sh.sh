ec19aefeb135634cc006b9881945a1d27df84a40
vbergam1 <115113062+vbergam1@users.noreply.github.com>
2022-10-18 17:08:01 -0300

    Update acender_leds_sh.sh

--- acender_leds_sh.sh
+++ acender_leds_sh.sh
@@ -2,9 +2,11 @@
 
 # Script para acender tres leds consecutivamente 5 vezes, sendo que a ordem é vermelho por 2 segundos, verde e amarelo por 1 segundo
 
-GPIO_amarelo_num = 16  # define o numero da porta associada ao led amarelo como uma variavel
-GPIO_vermelho_num = 20  # define o numero da porta associada ao led vermelho como uma variavel
-GPIO_verde_num = 21  # define o numero da porta associada ao led verde como uma variavel
+LED_amarelo = 16  # define o numero da porta associada ao led amarelo como uma variavel
+LED_vermelho = 20  # define o numero da porta associada ao led vermelho como uma variavel
+LED_verde = 21  # define o numero da porta associada ao led verde como uma variavel
+numero = 1
+
 
 definir_led (numero, GPIO_num)  {   # a variavel numero é passada para a funcao
 	echo $numero >> "/sys/class/gpio/gpio$GPIO_num/value"  
@@ -22,28 +24,28 @@ desabilitar (GPIO_num) { # funcao para desabilitar a porta
 
 
 function main() {  # funcao principal, responsavel por habilitar e desabilitar as portas, assim como acender e apagar os leds na ordem definida
-	habilitar (GPIO_amarelo_num)  # habilita a porta do led amarelo
-	habilitar (GPIO_vermelho_num)  # habilita a porta do led vermelho
-	habilitar (GPIO_verde_num)  # habilita a porta do led verde
+	habilitar (LED_amarelo)  # habilita a porta do led amarelo
+	habilitar (LED_vermelho)  # habilita a porta do led vermelho
+	habilitar (LED_verde)  # habilita a porta do led verde
 	c = 0   # define uma variavel que servira como um contador   
 	while [$c -lt 5]  # cria um looping, que para apos 5 repeticoes
 	do
-		definir_led(1, GPIO_vermelho_num)  # acende o led vermelho por 2 seg
+		definir_led(1, LED_vermelho)  # acende o led vermelho por 2 seg
 		sleep 2
-		definir_led(0, GPIO_vermelho_num)  # desliga o led vermelho
+		definir_led(0, LED_vermelho)  # desliga o led vermelho
 		sleep 0.1
-		definir_led(1, GPIO_verde_num)  # acende o led verde por 1 seg
+		definir_led(1, LED_verde)  # acende o led verde por 1 seg
 		sleep 1
-		definir_led(0, GPIO_verde_num)  # desliga o led verde
+		definir_led(0, LED_verde)  # desliga o led verde
 		sleep 0.1
-		definir_led(1, GPIO_amarelo_num)  # acende o led amarelo por 1 seg
+		definir_led(1, LED_amarelo)  # acende o led amarelo por 1 seg
 		sleep 1
-		definir_led(0, GPIO_amarelo_num)  # desliga o led amarelo
+		definir_led(0, LED_amarelo)  # desliga o led amarelo
 		sleep 0.1
 		c=c+1
 	done
-	desabilitar (GPIO_amarelo_num)  # desabilita a porta do led amarelo
-	desabilitar (GPIO_vermelho_num)  # desabilita a porta do led vermelho
-	desabilitar (GPIO_verde_num)  # desabilita a porta do led verde
+	desabilitar (LED_amarelo)  # desabilita a porta do led amarelo
+	desabilitar (LED_vermelho)  # desabilita a porta do led vermelho
+	desabilitar (LED_verde)  # desabilita a porta do led verde
 }
 main()  # chama e executa a funcao principal
