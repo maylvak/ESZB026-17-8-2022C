fd25ba16ecc2baa7da9e064e3942f43d1345b036
maylvak <mayam.lucena@gmail.com>
2022-11-16 18:18:16 -0200

    Modificacoes em aula

--- motor_dc.c
+++ motor_dc.c
@@ -7,23 +7,19 @@ int main() {
    int duty,divisor;                            // este programa deve ser rodado com 'sudo'
    wiringPiSetupGpio();                 // usa a numeracao da GPIO
    pinMode(pino_PWM0, PWM_OUTPUT);      // configura a GPIO18 com o PWM por hardware
-
+   pwmSetRange(1000);
+   pwmSetClock(38);
    // Ajustando a frequencia do PWM em 261,6 Hz com 1005 passos de duty cycle
    // frequencia PWM = 19,2 MHz / (divisor * range)
    // f = 19200000 / (divisor * range) => divisor = 73
    pwmSetMode(PWM_MODE_MS);             // usando frequencia fixa
-   for (duty = 4096; duty > 0; duty--){
-        divisor = 19200000 / (500 * duty);
-        pwmSetRange(duty);                    
-        pwmSetClock(divisor);  
+   for (duty = 1; duty < 1000; duty++){  
         printf("Testando motor. ");
         printf("Range = %d \n",duty);
-        pwmWrite(pino_PWM0, divisor);
-        usleep(10000);
-        pwmWrite(pino_PWM0, 0);
-        usleep(10000);
-  
-   }
+        pwmWrite(pino_PWM0, duty);
+	usleep(10000);
+    }
+   pwmWrite(pino_PWM0, 0);
    
    return 0;                            // a saida PWM permanece ligada apos o termino do programa
 }
